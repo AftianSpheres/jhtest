f10f92581ce4a9f1ce42325d4cbb2b2f26047932
Afti <yodamanyula@gmail.com>
2016-06-02 22:59:31 -0400

    Improved the feel of dodging, cleaned up that code into something marginally passable, and got the dodge booster passive effect fully implemented.

--- Assets/Scripts/Player/StateMachine/PlayerDodging.cs
+++ Assets/Scripts/Player/StateMachine/PlayerDodging.cs
@@ -1,19 +1,15 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System;
 
 public class PlayerDodging : StateMachineBehaviour
 {
     private Bounds[] roomColliders;
     private Collider2D collider;
     private PlayerController controller;
-    private static int basicDodgeFrameLength = 24;
-    private static int dodgeBoostLength = 12;
+    private static int basicDodgeFrameLength = 30;
+    private static int dodgeBoostLength = 10;
     private int dodgeBonus;
-    bool DirReleased;
     int FrameCtr;
 
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         controller = animator.gameObject.GetComponent<PlayerController>();
@@ -23,165 +19,78 @@ public class PlayerDodging : StateMachineBehaviour
         if ((controller.world.GameStateManager.heldPassiveItems & HeldPassiveItems.DodgeBooster) == HeldPassiveItems.DodgeBooster)
         {
             dodgeBonus = dodgeBoostLength;
+            animator.SetFloat("DodgeAnimsSpeedMulti", 2f);          
         }
         else
         {
             dodgeBonus = 0;
+            animator.SetFloat("DodgeAnimsSpeedMulti", 1f);
         }
         FrameCtr = 0;
-
-
     }
 
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-    //
-    //}
-
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        FrameCtr++;
-    }
-
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        animator.speed = 1.0f;
-    }
-
-    // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
     override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        FrameCtr++;
         Vector3 PosMod = new Vector3(0, 0, 0);
-        if (animator.GetBool("DodgeBurst") == true && ((FrameCtr > dodgeBoostLength / 3f && FrameCtr <= 2 * ((dodgeBoostLength + dodgeBonus) / 3f)) || FrameCtr % 2 == 0))
+        if (FrameCtr >= basicDodgeFrameLength + dodgeBonus)
+        {
+            animator.SetBool("DodgeBurst", false);
+        }
+        else if (animator.GetBool("DodgeBurst") == true && (FrameCtr % 2 == 0 || FrameCtr * 2 < basicDodgeFrameLength + dodgeBonus))
         {
             if (animator.GetCurrentAnimatorStateInfo(0).IsTag("FaceDown"))
             {
-                if (animator.GetBool("HeldLeft") == true)
-                {
-                    PosMod = new Vector3(-1, -1, 0);
-                }
-
-                else if (animator.GetBool("HeldRight") == true)
-                {
-                    PosMod = new Vector3(1, -1, 0);
-                }
-
-                else
-                {
-                    PosMod = new Vector3(0, -1, 0);
-                }
-
-                if (animator.GetBool("HeldDown") == true)
-                {
-                    PosMod = PosMod + new Vector3(0, -1, 0);
-                    if (FrameCtr >= basicDodgeFrameLength + dodgeBonus)
-                    {
-                        animator.SetBool("DodgeBurst", false);
-                    }
-                }
-                else if (FrameCtr >= basicDodgeFrameLength * (2 / 3))
-                {
-                    animator.SetBool("DodgeBurst", false);
-                }
+                PosMod = _in_RollInDirection(Vector3.down, animator, animator.GetBool("HeldDown"), true);
             }
             else if (animator.GetCurrentAnimatorStateInfo(0).IsTag("FaceUp"))
             {
-                if (animator.GetBool("HeldLeft") == true)
-                {
-                    PosMod = new Vector3(-1, 1, 0);
-                }
-
-                else if (animator.GetBool("HeldRight") == true)
-                {
-                    PosMod = new Vector3(1, 1, 0);
-                }
-
-                else
-                {
-                    PosMod = new Vector3(0, 1, 0);
-                }
-
-                if (animator.GetBool("HeldUp") == true)
-                {
-                    PosMod = PosMod + new Vector3(0, 1, 0);
-                    if (FrameCtr >= basicDodgeFrameLength + dodgeBonus)
-                    {
-                        animator.SetBool("DodgeBurst", false);
-                    }
-                }
-                else if (FrameCtr >= basicDodgeFrameLength * (2 / 3))
-                {
-                    animator.SetBool("DodgeBurst", false);
-                }
+                PosMod = _in_RollInDirection(Vector3.up, animator, animator.GetBool("HeldUp"), true);
             }
             else if (animator.GetCurrentAnimatorStateInfo(0).IsTag("FaceLeft"))
             {
-                if (animator.GetBool("HeldDown") == true)
-                {
-                    PosMod = new Vector3(-1, -1, 0);
-                }
-
-                else if (animator.GetBool("HeldUp") == true)
-                {
-                    PosMod = new Vector3(-1, 1, 0);
-                }
-
-                else
-                {
-                    PosMod = new Vector3(-1, 0, 0);
-                }
-
-                if (animator.GetBool("HeldLeft") == true)
-                {
-                    PosMod = PosMod + new Vector3(-1, 0, 0);
-                    if (FrameCtr >= basicDodgeFrameLength + dodgeBonus)
-                    {
-                        animator.SetBool("DodgeBurst", false);
-                    }
-                }
-                else if (FrameCtr >= basicDodgeFrameLength * (2/3))
-                {
-                    animator.SetBool("DodgeBurst", false);
-                }
+                PosMod = _in_RollInDirection(Vector3.left, animator, animator.GetBool("HeldLeft"), false);
             }
             else if (animator.GetCurrentAnimatorStateInfo(0).IsTag("FaceRight"))
             {
-                if (animator.GetBool("HeldDown") == true)
-                {
-                    PosMod = new Vector3(1, -1, 0);
-                }
-
-                else if (animator.GetBool("HeldUp") == true)
-                {
-                    PosMod = new Vector3(1, 1, 0);
-                }
-
-                else
-                {
-                    PosMod = new Vector3(1, 0, 0);
-                }
-
-                if (animator.GetBool("HeldRight") == true)
-                {
-                    PosMod = PosMod + new Vector3(1, 0, 0);
-                    if (FrameCtr >= basicDodgeFrameLength + dodgeBonus)
-                    {
-                        animator.SetBool("DodgeBurst", false);
-                    }
-                }
-                else if (FrameCtr >= basicDodgeFrameLength * (2f / 3))
-                {
-                    animator.SetBool("DodgeBurst", false);
-                }
+                PosMod = _in_RollInDirection(Vector3.right, animator, animator.GetBool("HeldRight"), false);
             }
         }
         ExpensiveAccurateCollision.CollideWithScenery(animator, roomColliders, PosMod, collider);
     }
 
-    // OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-    //
-    //}
+    private Vector3 _in_RollInDirection (Vector3 baseVector, Animator animator, bool input, bool moveVertically)
+    {
+        if (input == true)
+        {
+            baseVector = baseVector * 2;
+        }
+        else if (FrameCtr >= basicDodgeFrameLength)
+        {
+            animator.SetBool("DodgeBurst", false); // you can cancel dodges early if you have the dodge booster, so you're never forced into a longer animation because you're holding it
+        }
+        if (moveVertically == true)
+        {
+            if (animator.GetBool("HeldLeft") == true)
+            {
+                baseVector += Vector3.left;
+            }
+            else if (animator.GetBool("HeldRight") == true)
+            {
+                baseVector += Vector3.right;
+            }
+        }
+        else
+        {
+            if (animator.GetBool("HeldDown") == true)
+            {
+                baseVector += Vector3.down;
+            }
+            else if (animator.GetBool("HeldUp") == true)
+            {
+                baseVector += Vector3.up;
+            }
+        }
+        return baseVector;
+    }
 }
--- Assets/Scripts/Player/StateMachine/PlayerSlide.cs
+++ Assets/Scripts/Player/StateMachine/PlayerSlide.cs
@@ -1,23 +1,29 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class PlayerSlide : StateMachineBehaviour
 {
+    public int slideInterval;
     private int frameCtr;
     private Bounds[] roomColliders;
     private Collider2D collider;
+    private PlayerController master;
 
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        frameCtr = 0;
         roomColliders = animator.gameObject.GetComponent<PlayerController>().world.activeRoom.collision.allCollision;
         collider = animator.GetComponent<Collider2D>();
+        master = animator.GetComponent<PlayerController>();
+        if ((master.world.GameStateManager.heldPassiveItems & HeldPassiveItems.DodgeBooster) == HeldPassiveItems.DodgeBooster)
+        {
+            slideInterval = Mathf.FloorToInt(slideInterval / 2);
+        }
+        frameCtr = slideInterval - 1;
     }
 
 	override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         frameCtr++;
-        if (frameCtr == 1)
+        if (frameCtr == slideInterval)
         {
             frameCtr = 0;
             Vector3 PosMod = Vector3.zero;
@@ -25,19 +31,50 @@ public class PlayerSlide : StateMachineBehaviour
             switch (dir)
             {
                 case Direction.Down:
-                    PosMod = Vector3.down;
+                    PosMod = _in_SlideInDir(Vector3.down, Input.GetKey(master.world.PlayerDataManager.K_VertUp), true);
                     break;
                 case Direction.Up:
-                    PosMod = Vector3.up;
+                    PosMod = _in_SlideInDir(Vector3.up, Input.GetKey(master.world.PlayerDataManager.K_VertDown), true);
                     break;
                 case Direction.Left:
-                    PosMod = Vector3.left;
+                    PosMod = _in_SlideInDir(Vector3.left, Input.GetKey(master.world.PlayerDataManager.K_HorizRight), false);
                     break;
                 case Direction.Right:
-                    PosMod = Vector3.right;
+                    PosMod = _in_SlideInDir(Vector3.right, Input.GetKey(master.world.PlayerDataManager.K_HorizLeft), false);
                     break;
             }
             ExpensiveAccurateCollision.CollideWithScenery(animator, roomColliders, PosMod, collider);
         }
+    }
+
+    Vector3 _in_SlideInDir (Vector3 baseVector, bool input, bool moveVertically)
+    {
+        if (input == true)
+        {
+            baseVector = Vector3.zero;
+        }
+        else if (moveVertically == true)
+        {
+            if (Input.GetKey(master.world.PlayerDataManager.K_HorizLeft) == true)
+            {
+                baseVector += Vector3.left;
+            }
+            else if (Input.GetKey(master.world.PlayerDataManager.K_HorizRight) == true)
+            {
+                baseVector += Vector3.right;
+            }
+        }
+        else
+        {
+            if (Input.GetKey(master.world.PlayerDataManager.K_VertDown) == true)
+            {
+                baseVector += Vector3.down;
+            }
+            else if (Input.GetKey(master.world.PlayerDataManager.K_VertUp) == true)
+            {
+                baseVector += Vector3.up;
+            }
+        }
+        return baseVector;
     }
 }
