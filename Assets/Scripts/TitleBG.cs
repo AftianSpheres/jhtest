db973c500abd5e28d4faf3f1bbd3c603760d0162
Afti <yodamanyula@gmail.com>
2016-06-05 12:12:02 -0400

    the title was coming from inside the house the whole time

---
+++ Assets/Scripts/TitleBG.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleBG : MonoBehaviour
+{
+    private Vector3 originalTransform;
+    public int loopDistance;
+    public int scrollingSpeed;
+    int i;
+    private bool offFrame;
+	
+    void Start()
+    {
+        originalTransform = transform.position;
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (offFrame == false)
+        {
+            offFrame = true;
+            i += scrollingSpeed;
+            if (i >= loopDistance)
+            {
+                transform.position = originalTransform + (HammerConstants.LogicalResolution_Horizontal * Vector3.left);
+                i = HammerConstants.LogicalResolution_Horizontal;
+            }
+            else
+            {
+                transform.position += Vector3.left * scrollingSpeed;
+            }
+        }
+        else
+        {
+            offFrame = false;
+        }
+    }
+}
---
+++ Assets/TitleScreen.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class TitleScreen : MonoBehaviour
+{
+    public AudioSource BGM;
+    public GameObject curtain;
+    private bool inTransitionFromTitle = false;
+    private HardwareInterfaceManager hardwareInterfaceManager;
+    private float origVolume;
+
+	// Use this for initialization
+	void Start ()
+    {
+        GameObject hwIMobj = GameObject.Find("Universe/HardwareInterfaceManager");
+        if (hwIMobj != null)
+        {
+            hardwareInterfaceManager = hwIMobj.GetComponent<HardwareInterfaceManager>();
+        }    
+        origVolume = BGM.volume;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (hardwareInterfaceManager == null)
+        {
+            GameObject hwIMobj = GameObject.Find("Universe/HardwareInterfaceManager");
+            if (hwIMobj != null)
+            {
+                hardwareInterfaceManager = hwIMobj.GetComponent<HardwareInterfaceManager>();
+            }
+        }
+	    else if (hardwareInterfaceManager.Menu.BtnDown == true)
+        {
+            StartCoroutine(TransitionFromTitle(2));
+        }
+	}
+
+    IEnumerator TransitionFromTitle (int sceneID)
+    {
+        int i = 0;
+        while (i < 90)
+        {
+            BGM.volume = origVolume * ((90f - i) / 90f);
+            Debug.Log(BGM.volume);
+            if (i >= 45)
+            {
+                curtain.SetActive(true);
+            }
+            i++;
+            yield return null;
+        }
+        SceneManager.LoadScene(sceneID, LoadSceneMode.Single);
+    }
+}
